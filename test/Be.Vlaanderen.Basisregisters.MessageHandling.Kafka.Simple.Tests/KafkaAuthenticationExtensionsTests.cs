8ce39f71e658433928af6c930c0db1cf048a9430
Arne Dumarey <dumarey.arne@gmail.com>
2022-03-02 09:08:10 +0100

    fix: cleanup kafka auth

--- test/Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple.Tests/KafkaAuthenticationExtensionsTests.cs
+++ test/Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple.Tests/KafkaAuthenticationExtensionsTests.cs
@@ -4,16 +4,6 @@ namespace Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple.Tests
 
     public class KafkaAuthenticationExtensionsTests
     {
-        [Theory]
-        [InlineData("None", KafkaAuthentication.None)]
-        [InlineData("none", KafkaAuthentication.None)]
-        [InlineData("SaslPlainText", KafkaAuthentication.SaslPlainText)]
-        [InlineData("saslPlainText", KafkaAuthentication.SaslPlainText)]
-        [InlineData("", KafkaAuthentication.None)]
-        [InlineData(null, KafkaAuthentication.None)]
-        public void FromString(string value, KafkaAuthentication expected)
-        {
-            Assert.Equal(expected, value.FromString());
-        }
+        
     }
 }
