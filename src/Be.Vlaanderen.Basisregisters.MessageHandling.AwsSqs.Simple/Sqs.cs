b635a4de7effeac6b2174cc3d0adb66247e667b6
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-06-28 13:38:12 +0200

    fix: allow messageGroupId in CopyToQueue

--- src/Be.Vlaanderen.Basisregisters.MessageHandling.AwsSqs.Simple/Sqs.cs
+++ src/Be.Vlaanderen.Basisregisters.MessageHandling.AwsSqs.Simple/Sqs.cs
@@ -5,13 +5,17 @@ namespace Be.Vlaanderen.Basisregisters.MessageHandling.AwsSqs.Simple
 
     public static class Sqs
     {
-        public static async Task<bool> CopyToQueue<T>(SqsOptions sqsOptions, string queueName, T message, CancellationToken cancellationToken)
+        public static async Task<bool> CopyToQueue<T>(SqsOptions sqsOptions, string queueName, T message, string messageGroupId, CancellationToken cancellationToken)
             where T : class
         {
             var queueUrl = await SqsQueue.CreateQueueIfNotExists(sqsOptions, queueName, true, cancellationToken);
-            await SqsProducer.Produce(sqsOptions, queueUrl, message, string.Empty, cancellationToken);
+            await SqsProducer.Produce(sqsOptions, queueUrl, message, messageGroupId, cancellationToken);
 
             return true;
         }
+
+        public static async Task<bool> CopyToQueue<T>(SqsOptions sqsOptions, string queueName, T message, CancellationToken cancellationToken)
+            where T : class =>
+            await CopyToQueue(sqsOptions, queueName, message, string.Empty, cancellationToken);
     }
 }
